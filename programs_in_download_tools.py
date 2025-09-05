498cffc55e4f3e410268623a25ce6cc188edebab
John Quensen <quensenj@msu.edu>
2025-09-05 16:17:22 -0400

    Add list of programs downloaded by download_tools_jq.py

---
+++ programs_in_download_tools.py
@@ -0,0 +1,11 @@
+usearch 8.1  
+uchime4.2  
+infernal  
+fasttree  
+blast2  
+fungene pipeline  
+RDP assembler  
+gold.fa database  
+make Xander python scripts executable  
+exit Xander bash scripts  
+shown /home/RDPuser  
